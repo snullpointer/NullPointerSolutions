85640c4fa53c262f2ce6a1ac35113a3f6824bfdc
41471933 <m.a.schuster98@gmail.com>
2016-09-19 11:46:59 -0300

    Agregando sistema de envio de mail

--- email.php
+++ email.php
@@ -1,9 +1,13 @@
 <?php
 if($_POST){
-	$to      = 'nobody@example.com';
+	$to      = 'snullpointer@gmail.com';
 	$subject = $_POST['asunto'];
 	$message = 'De: '. $_POST['nombre'] . ' Email:'. $_POST['email'] . 'Mensaje: ' . $_POST['mensaje'];
 
-mail($to, $subject, $message);
+	echo $message;
+	/*
+	if(mail($to, $subject, $message){
+		
+	}*/
 }
 ?>
